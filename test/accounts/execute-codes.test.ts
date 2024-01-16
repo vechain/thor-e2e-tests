fd5c7c0ac4ced605d43f50a02e756d7351b4dc67
clayton neal <clayton.neal@vechain.org>
2024-01-16 10:56:17 +0000

    chore/account-names

--- test/accounts/execute-codes.test.ts
+++ test/accounts/execute-codes.test.ts
@@ -1,11 +1,11 @@
-import wallet from "../../src/wallet";
+import { wallet } from "../../src/wallet";
 import { Node1Client } from "../../src/thor-client";
 import assert from "node:assert"
 
 describe("POST /accounts/*", function () {
   it("should execute code", async function () {
-    const from = wallet["0x0f872421dc479f3c11edd89512731814d0598db5"];
-    const to = wallet["0x99602e4bbc0503b8ff4432bb1857f916c3653b85"];
+    const from = wallet("account2");
+    const to = wallet("account4");
 
     const res = await Node1Client.executeAccountBatch({
       clauses: [
