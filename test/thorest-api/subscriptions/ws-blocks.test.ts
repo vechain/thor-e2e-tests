556a9ff415f6c5a9330954ec2417d046311829e1
DoctorEenot <dreenot@gmail.com>
2024-07-08 13:34:17 +0300

    fix: it -> testCase

--- test/thorest-api/subscriptions/ws-blocks.test.ts
+++ test/thorest-api/subscriptions/ws-blocks.test.ts
@@ -1,26 +1,29 @@
 import { Client } from '../../../src/thor-client'
 import { components } from '../../../src/open-api-types'
+import { testCase } from '../../../src/test-case'
 
 /**
  * @group api
  * @group websockets
  */
 describe('WS /subscriptions/blocks', () => {
-    it('should be able to subscribe', async () => {
-        const beats: components['schemas']['SubscriptionBlockResponse'][] = []
 
-        const ws = Client.raw.subscribeToBlocks((newBlock) => {
-            beats.push(newBlock)
+    testCase(['testnet'])(
+        'should be able to subscribe', async () => {
+            const beats: components['schemas']['SubscriptionBlockResponse'][] = []
+
+            const ws = Client.raw.subscribeToBlocks((newBlock) => {
+                beats.push(newBlock)
+            })
+
+            await Client.raw.waitForBlock()
+            await Client.raw.waitForBlock()
+
+            //sleep for 1 sec to ensure the beat is received
+            await new Promise((resolve) => setTimeout(resolve, 1000))
+
+            expect(beats.length).toBeGreaterThanOrEqual(2)
+
+            ws.unsubscribe()
         })
-
-        await Client.raw.waitForBlock()
-        await Client.raw.waitForBlock()
-
-        //sleep for 1 sec to ensure the beat is received
-        await new Promise((resolve) => setTimeout(resolve, 1000))
-
-        expect(beats.length).toBeGreaterThanOrEqual(2)
-
-        ws.unsubscribe()
-    })
 })
