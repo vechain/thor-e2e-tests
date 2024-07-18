6c1c64378d9812d6d246c00c0a42f384a31e014d
Andrew Dobrinskii <andrei.dobrinskii@electiconsulting.com>
2024-07-18 14:52:16 +0300

    feat: fail tx, wrong chainId

--- test/thorest-api/transactions/post-transaction.test.ts
+++ test/thorest-api/transactions/post-transaction.test.ts
@@ -1,10 +1,14 @@
-import { ThorWallet } from '../../../src/wallet'
+import { Transaction } from '@vechain/sdk-core'
+import { Client } from '../../../src/thor-client'
+import { ThorWallet, generateAddress } from '../../../src/wallet'
+import { revertedPostTx } from './setup/asserts'
+import { TransactionDataDrivenFlow } from './setup/transaction-data-driven-flow'
 
 /**
  * @group api
  * @group transactions
  */
-describe('POST /transactions', function () {
+describe('POST /transactions', function() {
     const wallet = ThorWallet.txBetweenFunding()
 
     it.e2eTest('should send a transaction', 'all', async () => {
@@ -15,4 +19,40 @@ describe('POST /transactions', function () {
             'Transaction should not be reverted',
         ).toEqual(false)
     })
+
+    it.e2eTest(
+        'transaction should fail, wrong chain id',
+        'all',
+        async function() {
+            const receivingAddr = generateAddress()
+            const clauses = [
+                {
+                    value: 1,
+                    data: '0x',
+                    to: receivingAddr,
+                },
+            ]
+
+            const txBody = await wallet.buildTransaction(clauses)
+            txBody.chainTag = 0
+            const tx = new Transaction(txBody)
+            const signedTx = await wallet.signTransaction(tx)
+
+            const testPlan = {
+                postTxStep: {
+                    rawTx: signedTx.encoded.toString('hex'),
+                    expectedResult: (data: any) =>
+                        revertedPostTx(
+                            data,
+                            'bad tx: chain tag mismatch',
+                        ),
+                },
+
+            }
+
+            const ddt = new TransactionDataDrivenFlow(testPlan)
+            await ddt.runTestFlow()
+        },
+    )
+
 })
