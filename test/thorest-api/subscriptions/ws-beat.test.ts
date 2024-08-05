eb38eaa0a9898b65f97e12475a2b1cdc768001f6
Paolo Galli <paolo.galli@vechain.org>
2024-08-05 10:03:52 +0200

    test: add beat test

---
+++ test/thorest-api/subscriptions/ws-beat.test.ts
@@ -0,0 +1,75 @@
+import { Client } from '../../../src/thor-client'
+import assert from 'node:assert'
+import { components } from '../../../src/open-api-types'
+
+/**
+ * @group api
+ * @group websockets
+ * @group beats
+ */
+describe('WS /subscriptions/beat', () => {
+    it.e2eTest('should be able to retrieve blocks after the current best block when providing no position parameter', 'all', async () => {
+        const beats: components['schemas']['SubscriptionBeatResponse'][] = []
+
+        const bestBlock = await Client.raw.getBlock('best')
+        const ws = Client.raw.subscribeToBeats((newBlock) => {
+            beats.push(newBlock)
+        })
+
+        let attempts = 0
+        // wait for another block to be packed
+        while (beats.length === 0 && attempts < 6) {
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+            attempts++
+        }
+
+        assert(beats.length > 0, 'Best block not found')
+
+        const nextBestBlock = beats.find(beat => beat.number === (bestBlock.body?.number ?? 0) + 1)
+        assert(nextBestBlock, 'Block not found')
+        ws.unsubscribe()
+    })
+
+    it.e2eTest('should be able to retrieve 5 blocks in order', 'all', async () => {
+        const beats: components['schemas']['SubscriptionBeatResponse'][] = []
+
+        const genesis = await Client.raw.getBlock(0, false)
+
+        const ws = Client.raw.subscribeToBeats((newBlock) => {
+            beats.push(newBlock)
+        }, genesis.body?.id)
+
+        while (beats.length < 5) await new Promise((resolve) => setTimeout(resolve, 100))
+
+        expect(beats.length).toBeGreaterThanOrEqual(5)
+
+        for (let index = 0, expectedNumber = 1; index < 5; index++, expectedNumber++) {
+            expect(beats[index].number).toEqual(expectedNumber)
+        }
+        ws.unsubscribe()
+    })
+
+    it.e2eTest('should be able to get block A via api call and compare it with subscription result to check they are the same', 'all', async () => {
+        const beats: components['schemas']['SubscriptionBeatResponse'][] = []
+
+        const genesis = await Client.raw.getBlock(0, false)
+        const firstBlock = await Client.raw.getBlock(1, false)
+
+        const ws = Client.raw.subscribeToBeats((newBlock) => {
+            beats.push(newBlock)
+        }, genesis.body?.id)
+
+        //sleep for 100 millis to ensure the beat is received
+        await new Promise((resolve) => setTimeout(resolve, 100))
+
+        const firstBeat = beats.find(beat => beat.number === firstBlock.body?.number)
+
+        assert(firstBeat, 'Block not found')
+        expect(firstBeat?.id).toEqual(firstBlock.body?.id)
+        expect(firstBeat?.number).toEqual(firstBlock.body?.number)
+        expect(firstBeat?.timestamp).toEqual(firstBlock.body?.timestamp)
+        expect(firstBeat?.txsFeatures).toEqual(firstBlock.body?.txsFeatures)
+        expect(firstBeat?.parentID).toEqual(firstBlock.body?.parentID)
+        ws.unsubscribe()
+    })
+})
