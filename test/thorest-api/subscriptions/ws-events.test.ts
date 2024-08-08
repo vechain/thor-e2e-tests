e047b43a76dcc748a088e8b70e461cdcbf98169d
Makis Christou <michalis.christou@protonmail.com>
2024-08-08 09:48:45 +0300

    Add Websocket event tests

--- test/thorest-api/subscriptions/ws-events.test.ts
+++ test/thorest-api/subscriptions/ws-events.test.ts
@@ -5,14 +5,85 @@ import { addAddressPadding } from '../../../src/utils/padding-utils'
 import { components } from '../../../src/open-api-types'
 import { ThorWallet } from '../../../src/wallet'
 
+type SubscriptionParams = Record<string, string | undefined>
+
+const subscribeAndTestError = async (
+    params: SubscriptionParams,
+    expectedErrorMessage: string,
+) => {
+    Client.raw.subscribeToEvents(
+        () => {
+            fail(
+                `Callback should not be executed for an invalid parameter: ${JSON.stringify(params)}`,
+            )
+        },
+        params,
+        (error: { message: string }) => {
+            expect(error.message).toBe(expectedErrorMessage)
+        },
+    )
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+}
+
 /**
  * @group api
  * @group websockets
+ * @group events
  */
 describe('WS /subscriptions/event', () => {
-    it.e2eTest('should be able to subscribe', 'all', async () => {
+    it.e2eTest('should work for valid t0', 'all', async () => {
         const events: components['schemas']['SubscriptionEventResponse'][] = []
-        //const account = generateAddress()
+        const wallet = ThorWallet.txBetweenFunding()
+
+        Client.raw.subscribeToEvents(
+            (event) => {
+                events.push(event)
+            },
+            {
+                t0: interfaces.energy.getEvent('Transfer').topicHash,
+            },
+        )
+
+        const receipt = await wallet.waitForFunding()
+
+        //sleep for 1 sec to ensure the beat is received
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        const relevantEvent = events.find((event) => {
+            return event.meta?.txID === receipt?.meta?.txID
+        })
+
+        expect(relevantEvent).not.toBeUndefined()
+    })
+
+    it.e2eTest('should work for valid t1', 'all', async () => {
+        const events: components['schemas']['SubscriptionEventResponse'][] = []
+        const wallet = ThorWallet.txBetweenFunding(true)
+
+        Client.raw.subscribeToEvents(
+            (event) => {
+                events.push(event)
+            },
+            {
+                t1: addAddressPadding(wallet.address),
+            },
+        )
+
+        const receipt = await wallet.waitForFunding()
+
+        //sleep for 1 sec to ensure the beat is received
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        const relevantEvent = events.find((event) => {
+            return event.meta?.txID === receipt?.meta?.txID
+        })
+
+        expect(relevantEvent).not.toBeUndefined()
+    })
+
+    it.e2eTest('should work for valid t2', 'all', async () => {
+        const events: components['schemas']['SubscriptionEventResponse'][] = []
         const wallet = ThorWallet.txBetweenFunding()
 
         Client.raw.subscribeToEvents(
@@ -36,5 +107,131 @@ describe('WS /subscriptions/event', () => {
         })
 
         expect(relevantEvent).not.toBeUndefined()
+    })
+
+    it.e2eTest('should work for valid addr', 'all', async () => {
+        const events: components['schemas']['SubscriptionEventResponse'][] = []
+        const wallet = ThorWallet.txBetweenFunding()
+
+        Client.raw.subscribeToEvents(
+            (event) => {
+                events.push(event)
+            },
+            {
+                addr: contractAddresses.energy,
+            },
+        )
+
+        const receipt = await wallet.waitForFunding()
+
+        //sleep for 1 sec to ensure the beat is received
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        const relevantEvent = events.find((event) => {
+            return event.meta?.txID === receipt?.meta?.txID
+        })
+
+        expect(relevantEvent).not.toBeUndefined()
+    })
+
+    it.e2eTest('should work for valid position', 'all', async () => {
+        const events: components['schemas']['SubscriptionEventResponse'][] = []
+        const wallet = ThorWallet.txBetweenFunding()
+
+        const bestBlock = await Client.sdk.blocks.getBlockCompressed('best')
+        const bestBlockId = bestBlock?.id
+
+        Client.raw.subscribeToEvents(
+            (event) => {
+                events.push(event)
+            },
+            {
+                addr: contractAddresses.energy,
+                pos: bestBlockId,
+                t0: interfaces.energy.getEvent('Transfer').topicHash,
+                t2: addAddressPadding(wallet.address),
+            },
+        )
+
+        const receipt = await wallet.waitForFunding()
+
+        //sleep for 1 sec to ensure the beat is received
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        const relevantEvent = events.find((event) => {
+            return event.meta?.txID === receipt?.meta?.txID
+        })
+
+        expect(relevantEvent).not.toBeUndefined()
+    })
+
+    it.e2eTest('should get 2 notifications', 'all', async () => {
+        const eventTxs: { id: string }[] = []
+        const txpoolTxs: { id: string }[] = []
+
+        const wallet = ThorWallet.txBetweenFunding()
+
+        Client.raw.subscribeToEvents(
+            (event) => {
+                const txID = event.meta?.txID
+                if (txID) {
+                    eventTxs.push({ id: txID })
+                }
+            },
+            { addr: contractAddresses.energy },
+        )
+
+        Client.raw.subscribeToTxPool((txId) => {
+            txpoolTxs.push(txId)
+        })
+
+        const receipt = await wallet.waitForFunding()
+        const txId = receipt.meta?.txID
+
+        // Sleep for 1 sec to ensure the beat is received
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        const isInEventTxs = eventTxs.some((tx) => tx.id === txId)
+        const isInTxpoolTxs = txpoolTxs.some((tx) => tx.id === txId)
+
+        expect(eventTxs).not.toBeUndefined()
+        expect(txpoolTxs).not.toBeUndefined()
+        expect(isInEventTxs).toBeTrue()
+        expect(isInTxpoolTxs).toBeTrue()
+    })
+
+    it.e2eTest('should error for invalid position', 'all', async () => {
+        await subscribeAndTestError(
+            { pos: 'invalid position' },
+            'Unexpected server response: 400',
+        )
+    })
+
+    it.e2eTest('should error for out of range position', 'all', async () => {
+        const genesisBlock = await Client.sdk.blocks.getGenesisBlock()
+        const genesisBlockId = genesisBlock?.id
+
+        await subscribeAndTestError(
+            { pos: genesisBlockId },
+            'Unexpected server response: 403',
+        )
+    })
+
+    it.e2eTest('should error for invalid topic', 'all', async () => {
+        await subscribeAndTestError(
+            { t0: 'invalid topic' },
+            'Unexpected server response: 400',
+        )
+    })
+
+    it.e2eTest('should error for invalid address', 'all', async () => {
+        await subscribeAndTestError(
+            { addr: 'invalid address' },
+            'Unexpected server response: 400',
+        )
+    })
+
+    it.e2eTest('should error for empty query', 'all', async () => {
+        await subscribeAndTestError({}, 'Unexpected server response: 400')
     })
 })
