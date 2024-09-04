f22ab835bce5c609865a8da67ad9d291627fb58d
Paolo Galli <paolo.galli@vechain.org>
2024-09-04 15:49:20 +0200

    fix comment

--- network/config/health_check.sh
+++ network/config/health_check.sh
@@ -11,7 +11,7 @@ echo $response
 # Extract the value of "number" from the JSON response using grep
 number=$(echo $response | grep -o '"number":[^,}]*' | awk -F: '{print $2}' | tr -d '[:space:]')
 
-# Check if the number is greater than 1
+# Check if the number is greater than 0
 if [ $number -gt 0 ]; then
   echo "Health check passed! Number is greater than 0."
   exit 0
