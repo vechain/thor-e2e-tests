211c3e4e55e9a11f8e410f96b7315db09d7ab124
leszek-vechain <leszek.lorek@vechain.org>
2024-11-09 00:07:28 +0000

    chore(added headers):
    - added headers check
    - small changes to ws test

--- test/thorest-api/node/get-peers.test.js
+++ test/thorest-api/node/get-peers.test.js
@@ -7,6 +7,9 @@ import { Client } from '../../../src/thor-client'
 describe('GET /node/network/peers', () => {
     it.e2eTest('should get peers', 'all', async () => {
         const peers = await Client.raw.getPeers()
-        expect(peers.success, 'API Call should be a success').toBeTrue()
+
+        expect(peers.success).toEqual(true)
+        expect(peers.headers['x-thorest-ver']).toBeDefined()
+        expect(peers.headers['x-genesis-id']).toBeDefined()
     })
 })
