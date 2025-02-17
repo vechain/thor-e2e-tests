7ebce527cdc980883a74a9924be9dd16881ab272
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-02-17 11:12:40 +0000

    more tests

--- test/thorest-api/fees/get-fees-history.test.js
+++ test/thorest-api/fees/get-fees-history.test.js
@@ -1,4 +1,4 @@
-import { expect } from 'vitest'
+import { beforeEach, expect } from 'vitest'
 import { revisions } from '../../../src/constants'
 import { Client } from '../../../src/thor-client'
 import { HEX_AT_LEAST_1, HEX_REGEX_64 } from '../../../src/utils/hex-utils'
@@ -8,9 +8,57 @@ import { HEX_AT_LEAST_1, HEX_REGEX_64 } from '../../../src/utils/hex-utils'
  * @group fees
  */
 describe('GET /fees/history?blockCount={blockCount}?newestBlock={revision}', function () {
-    it.e2eTest('when newestBlock is a blockID', 'all', async () => {
+    let blockID, blockNumber
+
+    beforeEach(async () => {
         const block = await Client.raw.getBlock('best', false)
-        const blockID = block.body?.id
+        blockID = block.body?.id
+        blockNumber = block.body?.number
+    })
+
+    it.e2eTest('when newestBlock is a block ID', 'all', async () => {
+        const res = await Client.raw.getFeesHistory(1, blockID, false)
+
+        expect(res.success, 'API response should be a success').toBeTruthy()
+        expect(res.httpCode, 'Expected HTTP Code').toEqual(200)
+        const expectedRes = {
+            baseFees: expect.arrayContaining([
+                expect.stringMatching(HEX_AT_LEAST_1),
+            ]),
+            gasUsedRatios: expect.arrayContaining([expect.any(Number)]),
+            oldestBlock: expect.stringMatching(HEX_REGEX_64),
+        }
+        expect(res.body, 'Expected Response Body').toEqual(expectedRes)
+        expect(res.body.baseFees.length).toBe(1)
+        expect(res.body.gasUsedRatios.length).toBe(1)
+    })
+
+    it.e2eTest('when newestBlock is a block number', 'all', async () => {
+        const res = await Client.raw.getFeesHistory(1, blockNumber, false)
+
+        expect(res.success, 'API response should be a success').toBeTruthy()
+        expect(res.httpCode, 'Expected HTTP Code').toEqual(200)
+        const expectedRes = {
+            baseFees: expect.arrayContaining([
+                expect.stringMatching(HEX_AT_LEAST_1),
+            ]),
+            gasUsedRatios: expect.arrayContaining([expect.any(Number)]),
+            oldestBlock: expect.stringMatching(HEX_REGEX_64),
+        }
+        expect(res.body, 'Expected Response Body').toEqual(expectedRes)
+        expect(res.body.baseFees.length).toBe(1)
+        expect(res.body.gasUsedRatios.length).toBe(1)
+    })
+
+    it.e2eTest('when newestBlock is higher than best', 'all', async () => {
+        const res = await Client.raw.getFeesHistory(1, blockNumber + 10, false)
+
+        expect(res.success, 'API response should be a success').toBeFalsy()
+        expect(res.httpCode, 'Expected HTTP Code').toEqual(400)
+        expect(res.httpMessage, 'Expected Error Message').toEqual("newestBlock: not found\n")
+    })
+
+    it.e2eTest('when newestBlock is a blockID', 'all', async () => {
         const res = await Client.raw.getFeesHistory(1, blockID, false)
 
         expect(res.success, 'API response should be a success').toBeTruthy()
