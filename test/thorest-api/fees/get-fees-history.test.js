4888fc5d7aa39735a5fef592dbd83d6a2f31510b
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-02-17 16:01:03 +0000

    more tests

--- test/thorest-api/fees/get-fees-history.test.js
+++ test/thorest-api/fees/get-fees-history.test.js
@@ -126,23 +126,6 @@ describe('GET /fees/history?blockCount={blockCount}?newestBlock={revision}', fun
         },
     )
 
-    it.e2eTest('when newestBlock is a blockID', 'all', async () => {
-        const res = await Client.raw.getFeesHistory(1, blockID, false)
-
-        expect(res.success, 'API response should be a success').toBeTruthy()
-        expect(res.httpCode, 'Expected HTTP Code').toEqual(200)
-        const expectedRes = {
-            baseFees: expect.arrayContaining([
-                expect.stringMatching(HEX_AT_LEAST_1),
-            ]),
-            gasUsedRatios: expect.arrayContaining([expect.any(Number)]),
-            oldestBlock: expect.stringMatching(HEX_REGEX_64),
-        }
-        expect(res.body, 'Expected Response Body').toEqual(expectedRes)
-        expect(res.body.baseFees.length).toBe(1)
-        expect(res.body.gasUsedRatios.length).toBe(1)
-    })
-
     revisions.valid(true).forEach((revision) => {
         it.e2eTest(
             `Fees history: valid revision ${revision}`,
