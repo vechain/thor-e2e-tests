dcb06fef4f54c95711663259c1ac5b9784a4c025
Pedro Gomes <otherview@gmail.com>
2025-04-15 13:14:43 +0100

    Update the base fee errors (#88)

--- test/thorest-api/transactions/fork/galactica/post-tx-negative.test.js
+++ test/thorest-api/transactions/fork/galactica/post-tx-negative.test.js
@@ -40,7 +40,7 @@ describe('POST /transactions', () => {
             })
 
             const expectedErrMsg =
-                'tx rejected: max fee per gas is less than block base fee'
+                'tx rejected: gas price is less than block base fee: expected 10000000000000 got 9999999999999'
             expect(res.success).toBeFalsy()
             expect(res.httpMessage.trimEnd()).toBe(expectedErrMsg)
         },
@@ -100,7 +100,7 @@ describe('POST /transactions', () => {
             })
 
             const expectedErrMsg =
-                'tx rejected: max fee per gas is less than block base fee'
+                'tx rejected: gas price is less than block base fee: expected 10000000000000 got 9999999999999'
             expect(res.success).toBeFalsy()
             expect(res.httpMessage.trimEnd()).toBe(expectedErrMsg)
         },
