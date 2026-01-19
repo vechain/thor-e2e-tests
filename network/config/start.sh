8ebae181286c3105bd8f8a93d27d487227e6fda2
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2026-01-19 12:04:14 +0000

    chore: debug seed txs (#108)

--- network/config/start.sh
+++ network/config/start.sh
@@ -11,15 +11,17 @@ BOOTNODE_IP=$(ping -c 1 thor-disco | awk -F'[()]' '/PING/{print $2}')
 
 echo "$BOOTNODE_IP"
 
-thor \
-  --config-dir=/tmp \
+start_command="--config-dir=/tmp \
   --network /node/config/genesis.json \
-  --api-addr="0.0.0.0:8669" \
-  --api-cors="*" \
-  --api-allowed-tracers="all" \
+  --api-addr=0.0.0.0:8669 \
+  --api-cors=* \
+  --api-allowed-tracers=all \
   --verbosity=3 \
-  --bootnode "enode://e32e5960781ce0b43d8c2952eeea4b95e286b1bb5f8c1f0c9f09983ba7141d2fdd7dfbec798aefb30dcd8c3b9b7cda8e9a94396a0192bfa54ab285c2cec515ab@$BOOTNODE_IP:55555" \
+  --bootnode enode://e32e5960781ce0b43d8c2952eeea4b95e286b1bb5f8c1f0c9f09983ba7141d2fdd7dfbec798aefb30dcd8c3b9b7cda8e9a94396a0192bfa54ab285c2cec515ab@$BOOTNODE_IP:55555 \
   --api-enable-deprecated \
   --enable-metrics \
-  --metrics-addr 0.0.0.0:2112
+  --metrics-addr=0.0.0.0:2112"
 
+echo "Starting thor with command: thor $start_command"
+
+thor $start_command
